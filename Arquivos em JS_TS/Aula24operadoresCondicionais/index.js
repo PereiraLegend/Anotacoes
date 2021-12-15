1d788f7fcfc072c7e07f3eff3ad491347c073668
Lucas Pereira <pereirapro123@gmail.com>
2021-12-15 17:18:48 -0300

    Commit 15/12/21
    commit da tarde de JS

---
+++ Arquivos em JS_TS/Aula24operadoresCondicionais/index.js
@@ -0,0 +1,14 @@
+// if, else if, else
+const hora = 23;
+
+if (hora === 12) { // Posso usar < > <= >= != etc
+    console.log("Bom Dia");
+} else {
+    console.log("Vai dormir")
+}
+const tenhoGrana = false; // Aqui ele seleciona o boolean e dá o resultado conforme o mesmo
+if (tenhoGrana){
+    console.log("sim")
+} else {
+    console.log("Não")
+}
---
+++ Arquivos em JS_TS/Aula25operadoresCondicionaisp2/index.js
@@ -0,0 +1,9 @@
+// Se isso ocorrer, faça isso, senão faça isso {código}
+const numero = 2;
+if (numero >= 0 && numero <=  3) {
+    console.log("Sim o número é maior ou igual a zero");
+} else {
+    console.log("O Número está entre zero e três");
+}
+// uma vez que a engine do JS na estrutura condicional encontre um verdadeiro, mesmo sendo verdadeiro, ela vai sair da estrutura, mesmo não fazendo sentido, ou tendo nada a ver com o JS
+// para cada caso é um caso, procure separar as estruturas ifs conforme o que for requerido por ua variavel ou função
