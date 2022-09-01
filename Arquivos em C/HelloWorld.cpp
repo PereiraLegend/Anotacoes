e2757588622e94281c0d29276f98a0510ff68684
Lucas Pereira <pereirapro123@gmail.com>
2022-09-01 10:57:14 -0300

    Commit Anotacoes em C
    Inicio da revisão da linguagem em C

---
+++ Arquivos em C/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>//Chamando bibliotecas
+#include <stdlib.h>
+//Posso definir o main apenas, quando o programa começa ele executa tudo apartir do main, ou seja, o main é a base de tudo no código
+//main(){
+//	printf("Oi\n");
+//}
+
+//Ou posso definir o main como int como um método:
+int main(){
+	printf("Hello World!\n");//printf -> significa imprimir formatado
+	printf("Inicio do Curso de C, Relenbrando tudo\n");
+	return 0;
+}
+
