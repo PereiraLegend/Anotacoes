83772f02af674cca0f1c66ca2bd24288dd4a5bbc
Lucas Pereira <pereirapro123@gmail.com>
2022-09-28 21:30:39 -0300

    .

--- Arquivos em Python/AtividadesFaculdade/TrabalhoAV1/AtividadeAV1.py
+++ Arquivos em Python/AtividadesFaculdade/TrabalhoAV1/AtividadeAV1.py
@@ -1,21 +1,32 @@
 # Trabalho RAD em Python AV1
 
 #Remover possivél arquivo txt já criado
+"""
 import os
 if os.path.exists("AtividadesFaculdade/TrabalhoAV1/Arquivo.txt"):
     os.remove("Arquivo.txt")
 else:
     print("ESTO NON ECZISTE")
-
-Arquivo = open("Arquivo.txt", "w")
+"""
+Arquivo = open("Arquivo.txt", "w") # Colocar para criar arquivos
 #print(Arquivo.read())
 
+
 print("Insira as suas Informações:")
+
 Arquivo.write(input("Informe seu cpf:"))
+Arquivo.write(",")
 Arquivo.write(input("Informe o seu nome:"))
+Arquivo.write(",")
 Arquivo.write(input("Informe o seu sobrenome:"))
+Arquivo.write(",")
 Arquivo.write(input("Informe a sua Idade:"))
+Arquivo.write(",")
 Arquivo.write(input("Informe a sua Conta:"))
+Arquivo.write(",")
 
 
 Arquivo.close()
+
+# Plano : Tirar o que exclui o ultimo arquivo, colocar tudo o que pergunta em loop com comando para sair do loop e
+# encerrar o código, depois perguntar se quer abrir o arquivo gerado
