db80b24e8ff1eec8e16f5f635f22a0f374a861cc
Lucas Pereira <pereirapro123@gmail.com>
2022-10-05 20:44:26 -0300

    comit

--- Arquivos em Python/AtividadesFaculdade/TrabalhoAV1/AtividadeAV1.py
+++ Arquivos em Python/AtividadesFaculdade/TrabalhoAV1/AtividadeAV1.py
@@ -16,7 +16,7 @@ n = 1
 while n == 1:
     print("====================================================================")
     print("|Você deseja criar um novo arquivo ou abrir um já existente?")
-    selecionar = int(input("|Para Criar um Arquivo [1] - Para Abrir um Arquivo [2] - Para Alterar um Arquivo [3] - Para Deletar um arquivo [4] - Para sair [5]:"))
+    selecionar = int(input("|Para Criar um Arquivo [1] - Para Abrir um Arquivo [2] - Para Alterar um Arquivo [3] - Para Deletar um arquivo [4] - Para sair [5]: "))
 
     if selecionar == 1:
         print("====================================================================")
@@ -26,38 +26,38 @@ while n == 1:
         print("====================================================================")
         print("|--Insira as suas Informações --")
 
-        # Pessoa
-
-        Arquivo.write("-> Cpf: ")
+        #Parte da Pessoa
+        Arquivo.write("===============================")
+        Arquivo.write("|Cpf: ")
         Cpf = Arquivo.write(str(input("Informe seu cpf: ")) + "\n")
 
-        Arquivo.write("-> Nome: ")
+        Arquivo.write("|Nome: ")
         Nome = Arquivo.write(str(input("Informe o seu nome: ")) + "\n")
 
-        Arquivo.write("-> Sobrenome: ")
+        Arquivo.write("|Sobrenome: ")
         Sobrenome = Arquivo.write(str(input("Informe o seu sobrenome: ")) + "\n")
 
-        Arquivo.write("-> Idade: ")
+        Arquivo.write("|Idade: ")
         Idade = Arquivo.write(str(input("Informe a sua Idade: ")) + "\n")
 
-        Arquivo.write("-> Conta: ")
+        Arquivo.write("|Conta: ")
         Conta = Arquivo.write(str(input("Informe a sua Conta: ")) + "\n")
 
-        # Conta
+        # Parte da Conta
 
-        Arquivo.write("-> Agência: ")
+        Arquivo.write("|Agência: ")
         Agência = Arquivo.write(str(input("Informe a sua Agência: ")) + "\n")
 
-        Arquivo.write("-> Número: ")
+        Arquivo.write("|Número: ")
         Número = Arquivo.write(str(input("Informe o seu Número: ")) + "\n")
 
-        Arquivo.write("-> Saldo: ")
+        Arquivo.write("|Saldo: ")
         Saldo = Arquivo.write(str(input("Informe o seu Saldo: ")) + "\n")
 
-        Arquivo.write("-> Gerente: ")
+        Arquivo.write("|Gerente: ")
         Gerente = Arquivo.write(str(input("Informe o seu Gerente: ")) + "\n")
 
-        Arquivo.write("-> Titular: ")
+        Arquivo.write("|Titular: ")
         Titular = Arquivo.write(str(input("Informe a sua Titular: ")) + "\n")
 
         Arquivo.close()
@@ -65,9 +65,12 @@ while n == 1:
     if selecionar == 2:
         print("====================================================================")
         print("|Informe o arquivo a ser Aberto/Editado: ")
-        with open(input("|Informe o nome do arquivo a ser aberto [Obs.: Insira o formato do arquivo ex.: .txt no final]: "), "r+") as abrir: # Colocar para criar arquivos
-            for Abrir in abrir:
-                print(Abrir)
+        try:
+            with open(input("|Informe o nome do arquivo a ser aberto [Obs.: Insira o formato do arquivo ex.: .txt no final]: "), "r+") as abrir: # Colocar para criar arquivos
+                for Abrir in abrir:
+                    print(Abrir)
+        except:
+            print("Insira corretamente o nome do arquivo com o .txt no final!")
 
     if selecionar == 3:
 
@@ -79,58 +82,52 @@ while n == 1:
         print("====================================================================")
         print("|--Insira as suas Informações --")
 
-        # Pessoa
-
-        Arquivo.write("-> Cpf: ")
+        # Parte da Pessoa a ser editada
+        Arquivo.write("===============================")
+        Arquivo.write("|Cpf: ")
         Cpf = Arquivo.write(str(input("Informe seu cpf: ")) + "\n")
 
-        Arquivo.write("-> Nome: ")
+        Arquivo.write("|Nome: ")
         Nome = Arquivo.write(str(input("Informe o seu nome: ")) + "\n")
 
-        Arquivo.write("-> Sobrenome: ")
+        Arquivo.write("|Sobrenome: ")
         Sobrenome = Arquivo.write(str(input("Informe o seu sobrenome: ")) + "\n")
 
-        Arquivo.write("-> Idade: ")
+        Arquivo.write("|Idade: ")
         Idade = Arquivo.write(str(input("Informe a sua Idade: ")) + "\n")
 
-        Arquivo.write("-> Conta: ")
+        Arquivo.write("|Conta: ")
         Conta = Arquivo.write(str(input("Informe a sua Conta: ")) + "\n")
 
-        # Conta
+        # Parte da Conta a ser editada
 
-        Arquivo.write("-> Agência: ")
+        Arquivo.write("|Agência: ")
         Agência = Arquivo.write(str(input("Informe a sua Agência: ")) + "\n")
 
-        Arquivo.write("-> Número: ")
+        Arquivo.write("|Número: ")
         Número = Arquivo.write(str(input("Informe o seu Número: ")) + "\n")
 
-        Arquivo.write("-> Saldo: ")
+        Arquivo.write("|Saldo: ")
         Saldo = Arquivo.write(str(input("Informe o seu Saldo: ")) + "\n")
 
-        Arquivo.write("-> Gerente: ")
+        Arquivo.write("|Gerente: ")
         Gerente = Arquivo.write(str(input("Informe o seu Gerente: ")) + "\n")
 
-        Arquivo.write("-> Titular: ")
+        Arquivo.write("|Titular: ")
         Titular = Arquivo.write(str(input("Informe a sua Titular: ")) + "\n")
 
         Arquivo.close()
 
     if selecionar == 4:
         print("====================================================================")
-        while p == 1:
-            try:
-                os.remove(str(input("Informe o nome do arquivo para ser deletado: ")))
-                print("Seu arquivo foi deletado com Sucesso!")
-                p == 1
+        try:
+            os.remove(str(input("Informe o nome do arquivo para ser deletado: ")))
+            print("Seu arquivo foi deletado com Sucesso!")
+        except:
+            print("O nome foi escrito de forma incorreta! Digite novamente")
 
-            except:
-                print("O nome foi escrito de forma incorreta! Digite novamente")
 
-    else:
-        print("====================================================================")
-        n = int(input("|Você deseja voltar ao menu ou  sair? Menu [1] Sair [2]: "))
+    print("====================================================================")
+    n = int(input("|Você deseja voltar ao menu ou  sair? Menu [1] Sair [2]: "))
 
 # banco de dados, procurar função para deletar os arquivos criados e corrigir o erro do loop
-
-
-# AVVVAAAAA
