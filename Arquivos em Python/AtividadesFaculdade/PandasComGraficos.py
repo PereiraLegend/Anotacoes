1104414dd490a0c37b39ce3f53c91e0059c54f6e
Lucas Pereira <pereirapro123@gmail.com>
2022-11-05 19:35:18 -0300

    Commit estudo/ tb bigData

--- Arquivos em Python/AtividadesFaculdade/PandasComGraficos.py
+++ Arquivos em Python/AtividadesFaculdade/PandasComGraficos.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 plt.close("all")
 serie_temporal = pd.Series(np.random.randn(1000), index = pd.date_range("28/10/2022", periods=1000))
