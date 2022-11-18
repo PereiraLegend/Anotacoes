da43e6334c077f3291852a1e839fbf30c5091d8d
Lucas Pereira <pereirapro123@gmail.com>
2022-11-18 11:00:20 -0300

    Commit Anotacoes

---
+++ Arquivos em Python/AtividadesFaculdade/Exemplos/plotando.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+dados = pd.read_csv("winequality-red.csv", sep=';')
+
+"""
+A última coluna se refere à classe. Observando os dados é possível ver que as classes são representadas por números
+inteiros. Para saber quais são os valores possíveis e únicos, está sendo utilizado o método 'unique' do numpy. Esse
+método é aplicado sobre a última coluna, cujo nome é 'quality'.
+"""
+classes = np.unique(dados['quality'])
+
+# Pegando a quantidade de atributos. O -1 é para retirar o atributo que na verdade é a classe.
+num_attr = len(dados.columns) - 1
+
+attr1 = np.random.randint(num_attr)
+attr2 = np.random.randint(num_attr)
+# Caso o segundo seja igual ao primeiro
+while attr2 == attr1:
+    attr2 = np.random.randint(num_attr)
+
+# Gerando um plot para cada classe
+for i in range(len(classes)):
+    # Pegando todas as observações da mesma classe.
+    classe = dados.loc[dados['quality'] == classes[i]]
+
+    """
+    Para ver outros marcadores possíveis: documentação ou tutoriais online: 
+    https://www.geeksforgeeks.org/matplotlib-markers-module-in-python/
+    Na código abaixo estou selecionando todas as linhas da coluna 'attr1' para o eixo x, e todas as linhas da coluna
+    'attr2' como eixo y. O marcador está configurado para ser um ponto, e os rótulos serão os valores das classes.
+    """
+    plt.scatter(classe.iloc[:, attr1], classe.iloc[:, attr2], marker=".", label=classes[i])
+
+# Configurando para aparecer o attr1 no eixo x, e o attr2 no eixo y
+plt.xlabel(dados.columns.values[attr1])
+plt.ylabel(dados.columns.values[attr2])
+# Para aparecer a legenda. Neste caso será o mapeamento de cada classe para cada cor
+plt.legend()
+plt.show()
--- Arquivos em Python/AtividadesFaculdade/TrabalhoPandasAV2/TrabalhoBDPAV2.py
+++ Arquivos em Python/AtividadesFaculdade/TrabalhoPandasAV2/TrabalhoBDPAV2.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -1250,34 +1252,62 @@ print("========================================================================"
 
 #Gerando gráficos
 
-plt.scatter(iris_df["5.1"],iris_df["3.5"],color = "black", marker="o")
-pyplot.xlabel("5.1")
-pyplot.ylabel("3.5")
+iris_rX = ["5.1","3.5","1.4","0.2"]
+iris_rXX = random.sample(iris_rX,1)
+iris_rY = ["5.1","3.5","1.4","0.2"]
+iris_rYY = random.sample(iris_rY,1)
+plt.scatter(iris_df[iris_rXX],iris_df[iris_rYY],color = "black", marker="o")
+pyplot.xlabel(iris_rXX)
+pyplot.ylabel(iris_rYY)
 plt.title("Iris.data")
+plt.legend([iris_rXX,iris_rYY])
 plt.show()
 
-plt.scatter(pedalmeedges_df["weight"],pedalmeedges_df["from"],pedalmeedges_df["to"],color = "red", marker="*")
-pyplot.xlabel("weight")
-pyplot.ylabel("from")
-pyplot.xlabel("to")
-plt.title("Pedalme_edges.csv")
-plt.show()
 
-plt.scatter(wine_df["1"],wine_df["1.71"],color = "blue", marker="+")
-pyplot.xlabel("1")
-pyplot.ylabel("1.71")
+wine_rX = ["1","14.23",'1.71','2.43','15.6','127','2.8','3.06','.28','2.29','5.64','1.04','3.92','1065']
+wine_rXX = random.sample(wine_rX,1)
+wine_rY = ["1","14.23",'1.71','2.43','15.6','127','2.8','3.06','.28','2.29','5.64','1.04','3.92','1065']
+wine_rYY = random.sample(wine_rY,1)
+plt.scatter(wine_df[wine_rXX],wine_df[wine_rYY],color = "blue", marker="+")
+pyplot.xlabel(wine_rXX)
+pyplot.ylabel(wine_rYY)
 plt.title("Wine.data")
-
+plt.legend([wine_rXX,wine_rYY])
 plt.show()
 
-plt.scatter(accelerometer_df["x"],accelerometer_df["y"],color = "yellow", marker="o")
-pyplot.xlabel("x")
-pyplot.ylabel("y")
+
+accelerometer_rX = ['wconfid','pctid','x','y','z']
+accelerometer_rXX = random.sample(accelerometer_rX,1)
+accelerometer_rY = ['wconfid','pctid','x','y','z']
+accelerometer_rYY = random.sample(accelerometer_rY,1)
+plt.scatter(accelerometer_df[accelerometer_rXX],accelerometer_df[accelerometer_rYY],color = "yellow", marker="o")
+pyplot.xlabel(accelerometer_rXX)
+pyplot.ylabel(accelerometer_rYY)
 plt.title("Accelerometer.csv")
+plt.legend([accelerometer_rXX,accelerometer_rYY])
 plt.show()
 
-plt.scatter(tetuancity_df["Temperature"],tetuancity_df["Humidity"],color = "green", marker="*")
-pyplot.xlabel("Temperature")
-pyplot.ylabel("Humidity")
+
+tetuancity_rX = ['Temperature','Humidity','Wind Speed','general diffuse flows','diffuse flows','Zone 1 Power Consumption','Zone 2  Power Consumption','Zone 3  Power Consumption']
+tetuancity_rXX = random.sample(tetuancity_rX,1)
+tetuancity_rY = ['Temperature','Humidity','Wind Speed','general diffuse flows','diffuse flows','Zone 1 Power Consumption','Zone 2  Power Consumption','Zone 3  Power Consumption']
+tetuancity_rYY = random.sample(tetuancity_rY,1)
+plt.scatter(tetuancity_df[tetuancity_rXX],tetuancity_df[tetuancity_rYY],color = "green", marker="*")
+pyplot.xlabel(tetuancity_rXX)
+pyplot.ylabel(tetuancity_rYY)
 plt.title("Tetuan City power consuption.csv")
+plt.legend([tetuancity_rXX,tetuancity_rYY])
 plt.show()
+
+
+pedalmeedges_rX = ['from','to','weight']
+pedalmeedges_rXX = random.sample(pedalmeedges_rX,1)
+pedalmeedges_rY = ['from','to','weight']
+pedalmeedges_rYY = random.sample(pedalmeedges_rY,1)
+plt.scatter(pedalmeedges_df[pedalmeedges_rXX],pedalmeedges_df[pedalmeedges_rYY], marker="*")
+pyplot.xlabel(pedalmeedges_rXX)
+pyplot.ylabel(pedalmeedges_rYY)
+plt.title("Pedalme_edges.csv")
+plt.legend([pedalmeedges_rXX,pedalmeedges_rYY])
+plt.show()
+
