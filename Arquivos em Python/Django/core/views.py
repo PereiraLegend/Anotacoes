5ddc71a475d61382e94883d97f87c3916bec4c7b
PereiraLegend <pereirapro123@gmail.com>
2023-02-16 10:46:46 -0300

    Commit Django aula

--- Arquivos em Python/Django/core/views.py
+++ Arquivos em Python/Django/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Produto
 # Create your views here.
 # Quando trocamos o debug para false ele vem direto para essa página e essa função procurado templates
 def index(request):
+
+    """
     print(request)
     print(dir(request))
     print(f'User.Agent: {request.headers["User-Agent"]}')
@@ -13,12 +15,21 @@ def index(request):
         teste = "Usuário não logado"
     else:
         teste = "Usuário logado"
+
+
     context = {
         'curso': 'Programação Web com Django Framework',
         'outro': 'Outro Djkango',
         'logado': teste
     }
     return render(request, "index.html", context) # Aqui defino templates
-
+    """
+    produtos = Produto.objects.all()
+    context = {
+        'curso': 'Programação Web com Django Framework',
+        'outro': 'Outro Djkango',
+        'produtos': produtos
+    }
+    return render(request, "index.html", context)  # Aqui defino templates
 def contato(request):
     return render(request, "contato.html") # Aqui crio mais uma página html
