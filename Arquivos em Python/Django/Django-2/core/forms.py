baac910e7467cebc7e76fa791f12156fd3b67c4b
PereiraLegend <pereirapro123@gmail.com>
2023-02-26 13:28:22 -0300

    Commit aula

--- Arquivos em Python/Django/Django-2/core/forms.py
+++ Arquivos em Python/Django/Django-2/core/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class ContatoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
     email = forms.EmailField(label='E-mail', max_length=100)
-    assunto = forms.CharField(label = 'E-mail', max_length=120)
+    assunto = forms.CharField(label = 'Assunto', max_length=120)
     mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea()) # Textarea é uma caixa de texto com várias linhas
 
--- Arquivos em Python/Django/Django-2/core/views.py
+++ Arquivos em Python/Django/Django-2/core/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-
+from django.contrib import messages #Serve para mandar mensagens no bootstrap
 from .forms import ContatoForm
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    form = ContatoForm()
+    form = ContatoForm(request.POST or None)
+    # Eu preciso saber se o método é post ou get
+    if str(request.method) == 'POST':
+        print(f'Post: {request.POST}') # Inprime o que é mandado com a mensagem
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: {nome}')
+            print(f'E-mail: {email}')
+            print(f'Assunto: {assunto}')
+            print(f'Mensagem: {mensagem}')
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail')
     context = {
         'form': form
     }
