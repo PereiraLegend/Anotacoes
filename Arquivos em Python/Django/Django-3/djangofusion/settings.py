fd5864b732364d53306b76185bd33a958e37ca65
PereiraLegend <pereirapro123@gmail.com>
2023-03-17 10:12:39 -0300

    Commit aula

--- Arquivos em Python/Django/Django-3/djangofusion/settings.py
+++ Arquivos em Python/Django/Django-3/djangofusion/settings.py
@@ -76,11 +76,8 @@ WSGI_APPLICATION = 'djangofusion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgreesql',
-        'NAME': 'djangofusion',
-        'USER': 'lucas',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
