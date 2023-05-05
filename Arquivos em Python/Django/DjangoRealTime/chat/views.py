de9c1edbbc1981caf6733f620f6580840a43bc1d
PereiraLegend <pereirapro123@gmail.com>
2023-05-04 21:20:19 -0300

    Commit projeto chat

--- Arquivos em Python/Django/DjangoRealTime/chat/views.py
+++ Arquivos em Python/Django/DjangoRealTime/chat/views.py
@@ -1,3 +1,19 @@
+from typing import Any, Dict
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from django.utils.safestring import mark_safe
+import json
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class SalaView(TemplateView):
+    template_name = 'sala.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SalaView,self).get_context_data(**kwargs)
+        context['nome_sala_jason'] = mark_safe(
+            json.dumps(self.kwargs['nome_sala'])
+        )
+        return context
