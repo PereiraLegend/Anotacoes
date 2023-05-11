48d0d0c6fa7d3edcfe82a184ea345520a13ade33
PereiraLegend <pereirapro123@gmail.com>
2023-05-11 10:46:43 -0300

    Commkit

--- Arquivos em Python/Django/DjangoRealTime/chat/urls.py
+++ Arquivos em Python/Django/DjangoRealTime/chat/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import IndexView, SalaView
 urlpatterns = [
-    path('', Index.as_view(), name='index'),
-    path('chat/str:nome_sala/', SalaView.as_view(), name='sala'),
+    path('', IndexView.as_view(), name='index'),
+    path('chat/<str:nome_sala>/', SalaView.as_view(), name='sala'),
 ]
