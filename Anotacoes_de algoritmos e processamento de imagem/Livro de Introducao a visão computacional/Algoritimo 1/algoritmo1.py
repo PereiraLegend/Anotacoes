c376b7fc6a3297142abc1199f31bb633fe6b47a0
Lucas Pereira <pereirapro123@gmail.com>
2023-08-22 18:18:23 -0300

    Commit

---
+++ Anotacoes_de algoritmos e processamento de imagem/Livro de Introducao a visão computacional/Algoritimo 1/algoritmo1.py
@@ -0,0 +1,14 @@
+# Importação das bibliotecas
+
+import cv2
+# Leitura da imagem com a função imread()
+imagem = cv2.imread('entrada.jpg')
+print('Largura em pixels: ', end='')
+print(imagem.shape[1]) #largura da imagem
+print('Altura em pixels: ', end='')
+print(imagem.shape[0]) #altura da imagem
+print('Qtde de canais: ', end='')
+print(imagem.shape[2])
+# Mostra a imagem com a função imshow
+cv2.imshow("Nome da janela", imagem)
+cv2.waitKey(0) #espera pressionar 
