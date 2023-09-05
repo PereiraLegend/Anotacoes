1571093e1959385267c18ee96f8265da0ba08dd4
Lucas Pereira <pereirapro123@gmail.com>
2023-09-05 14:12:54 -0300

    Commit

--- Arquivos em Dart/Aula 1/aula5.dart
+++ Arquivos em Dart/Aula 1/aula5.dart
@@ -3,15 +3,64 @@
 //Obs.: em resumo é um contrato que alguém deve seguir
 void main(){
 
+
+  //Parte de herança
+  David david = David();
+  david.falar();
+  print(david.falar()); //Aqui eu consigo acessar o ".falar()" pela herança da classe "pai" para a classe "filho"
+
+  //Parte Polimorfismo
+  // No polimorfismo o objeto pode assumir diferentes respostas
+  Pagamento pagamento = PagarComBoleto();
+  pagamento.pagar();
+
+  pagamento = PagarComPix();
+  pagamento.pagar();
 }
 
-abstract class Pessoa{
-  String comunicar();
+//Abstração
+abstract class Pessoa{ //Aqui crio uma classe abstrata
+  String comunicar(); // Eu não faço a implementação disso // Isso é um método
 
 }
 
-class PessoaET implements Pessoa {
+class PessoaET implements Pessoa { //Aqui eu implemento a Pessoa a PEssoaET
   String comunicar() {
-    return
+    return "Olá Mundo";
+  }
+}
+
+class PessoaNaoET implements Pessoa {
+  String comunicar(){
+    return "Bom Dia";
+  }
+}
+
+//Herança
+class Pai{
+  String falar(){
+    return "Girias";
+  }
+}
+
+class David extends Pai { // Aqui eu extendo uma classe para herdar as suas caracteristicas
+
+}
+
+//Polimosfismo
+
+abstract class Pagamento {
+  void pagar();
+}
+
+class PagarComBoleto implements Pagamento {
+  void pagar(){
+    print("Pagando com boleto");
+  }
+}
+
+class PagarComPix implements Pagamento {
+  void pagar(){
+    print("Pagando com pix");
   }
 }
