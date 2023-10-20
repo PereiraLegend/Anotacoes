e7ef86165d6d5cfab9294dc68f71cb68aa423ce9
Lucas <pereirapro123@gmail.com>
2023-10-20 18:07:19 -0300

    Commit trabalho

--- Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/teste.py
+++ Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/teste.py
@@ -1,31 +1,31 @@
-print("Hello World!")
-def bissecao(f, a, b, precisao=1e-6, max_iter=100):
-    if f(a) * f(b) >= 0:
-        raise ValueError("A função deve mudar de sinal em [a, b]")
+def funcao(x):
+    return x**3 - 2*x**2 - 5
+
+def bissecao(funcao, a, b, precisao=1e-6):
+    if funcao(a) * funcao(b) >= 0:
+        raise ValueError("A função deve mudar de sinal em [a, b].")
     
-    iteracao = 0
-    while (b - a) > precisao and iteracao < max_iter:
+    while (b - a) > precisao:
         c = (a + b) / 2
-        if f(c) == 0:
+        valor_c = funcao(c)
+        
+        if valor_c == 0:
             return c
-        if f(a) * f(c) < 0:
+        elif valor_c * funcao(a) < 0:
             b = c
         else:
             a = c
-        iteracao += 1
-
+    
     return (a + b) / 2
 
-# Exemplo de uso:
-# Defina a função que você deseja encontrar a raiz
-def funcao(x):
-    return x**3 - 5*x**2 + 2
+# Intervalo inicial [a, b] onde a função muda de sinal
+a = 1
+b = 3
 
-# Defina o intervalo inicial [a, b] onde a função muda de sinal
-a = 0
-b = 2
+# Precisão desejada
+#precisao = 1e-6
 
 # Chame a função de bissecção para encontrar a raiz
-raiz = bissecao(funcao, a, b)
+raiz = bissecao(funcao, a, b, 1e-6)
 
 print(f'A raiz da função é aproximadamente: {raiz:.6f}')
--- Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/teste3.py
+++ Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/teste3.py
@@ -1,12 +1,12 @@
 def funcao(x):
     return x**2 + 2*x - 4
 
-def bissecao(funcao, x, y, p=1e-5, max=100):
+def bissecao(funcao, x, y, p=1e-5, max_iter=100):
     if funcao(x) * funcao(y) >= 0:
         print("A função não cumpre os critérios do método da bisseção.")
         return None
     ni = 0
-    while (y - x) / 2.0 > p and ni < max:
+    while (y - x) / 2.0 > p and ni < max_iter:
         c = (x + y) / 2.0
         if funcao(c) == 0:
             return c
@@ -21,6 +21,6 @@ x = 0
 y = 3
 r = bissecao(funcao, x, y)
 if r is not None:
-    print(f"A raiz da é aproximadamente: {r:.5f}")
+    print(f"A raiz é aproximadamente: {r:.5f}")
 else:
     print("Raiz não encontrada!")
