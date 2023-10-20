582bbc7399d2be9143ed435aebea58f616884e76
Lucas <pereirapro123@gmail.com>
2023-10-20 17:26:09 -0300

    Commit

--- Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/Atividade2.py
+++ Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/Atividade2.py
@@ -1,31 +1,27 @@
 def metodoBissecao(num, precisao=1e-6):
-    #numero = float(numero)  # Converte a entrada em um número de ponto flutuante
-
     if num < 0:
         raise ValueError("A raiz quadrada de números negativos não é real.")
     
     if num == 0:
         return 0
     
-    # Defina os limites iniciais do intervalo
+    if precisao <= 0:
+        raise ValueError("A precisão deve ser um valor positivo.")
+    
     a, b = 0, max(num, 1)
     
     while (b - a) > precisao:
-        # Encontre o ponto médio do intervalo
         x = (a + b) / 2
-        
-        # Calcule o valor da função no ponto médio
         f_x = x * x - num
         
-        if f_x == 0:
-            return x  # Encontramos a raiz exata
+        if abs(f_x) < precisao:
+            return x
         
         if f_x < 0:
             a = x
         else:
             b = x
     
-    # Retorne o ponto médio como a raiz aproximada
     return (a + b) / 2
 
 # Exemplo de uso
--- Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/teste.py
+++ Arquivos em Python/AtividadesFaculdade/Atividade Modelagem Matemática/teste.py
@@ -1,1 +1,31 @@
 print("Hello World!")
+def bissecao(f, a, b, precisao=1e-6, max_iter=100):
+    if f(a) * f(b) >= 0:
+        raise ValueError("A função deve mudar de sinal em [a, b]")
+    
+    iteracao = 0
+    while (b - a) > precisao and iteracao < max_iter:
+        c = (a + b) / 2
+        if f(c) == 0:
+            return c
+        if f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+        iteracao += 1
+
+    return (a + b) / 2
+
+# Exemplo de uso:
+# Defina a função que você deseja encontrar a raiz
+def funcao(x):
+    return x**3 - 5*x**2 + 2
+
+# Defina o intervalo inicial [a, b] onde a função muda de sinal
+a = 0
+b = 2
+
+# Chame a função de bissecção para encontrar a raiz
+raiz = bissecao(funcao, a, b)
+
+print(f'A raiz da função é aproximadamente: {raiz:.6f}')
