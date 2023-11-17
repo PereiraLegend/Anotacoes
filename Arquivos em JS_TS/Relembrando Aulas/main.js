53b8501714678edfcef0f29c7defe24273bda142
Lucas <pereirapro123@gmail.com>
2023-11-17 13:39:14 -0300

    Commit atualizações

---
+++ Arquivos em JS_TS/Relembrando Aulas/main.js
@@ -0,0 +1,29 @@
+/*alert("Oi")*/
+//Definindo um objeto:
+/*
+const circulo = {
+    radius: 1,
+    localizacao: {
+        x: 1,
+        y: 1
+    },
+    desenhar: function() {
+        console.log('desenhar')
+    }
+}
+
+circulo.desenhar()
+*/
+
+const circulo = {
+    radius: 1,
+    localizacao: {
+        x: 1,
+        y: 1
+    },
+    desenhar: function() {
+        console.log('desenhar')
+    }
+}
+
+circulo.desenhar()
