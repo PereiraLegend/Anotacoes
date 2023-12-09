6abf8101b35c9702a5ff0c696a39c91eb222873e
Lucas <pereirapro123@gmail.com>
2023-12-09 12:31:05 -0300

    Commit

--- Anotacoes Next/aprendendo-next/app/dashboard/page.tsx
+++ Anotacoes Next/aprendendo-next/app/dashboard/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-
-interface PageProps {}
-
-const Page: React.FC<PageProps> = () => {
-   return (<div>Page</div>)
+const page = async ({}) => {
+   const res = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      // No caso aqui eu armazeno os dados em cache e os revalido em determinados espaços de tempo, o que evita puxa-los toda vez, o que faz com que eu economize 
+      cache: 'no-store', // Aqui eu forço a pegar o cach ou fazer outro tipo de manipulação com ele
+   })
+   const data = await res.json()
+   return (
+      <div>
+         <div>dashboard</div>
+         {/**Renderizando o json */}
+         <div>{JSON.stringify(data)}</div>
+      </div>
+   )
 }
-
-export default Page;
+export default page
--- Anotacoes Next/aprendendo-next/app/post/[postId]/page.tsx
+++ Anotacoes Next/aprendendo-next/app/post/[postId]/page.tsx
@@ -1,11 +1,76 @@
-import { FC } from 'react'
+/*
+import React from 'react';
 
-interface PageProps {}
+interface PageProps{
+    params: {
+        postId: string
+    }
+}
 
-const page: FC<PageProps> = ({}) => {
+function PostPage({ postId }) {
+  // Lógica do componente
+  // Geralmente aqui é retornado a lógica do banco de dados
+  return (
+    <div>
+      <h1>Post {postId}</h1>
+      {/* Conteúdo do post *}
+
+    </div>
+  );
+}
+
+export default PostPage;
+*/
+/*
+// Agora a lógica de postagem :
+import react from "react"
+interface PageProps {
+    params: {
+        postId: string
+    }
+}
+
+const page = async ({params} : PageProps) => {
+    // Lógica do banco de dados
+
     return (
         <div>
-            
+            {/* Utilizando-se da lógica de servidor aqui ele retorna o que for digitado depois do post\...  *}
+            <div>{params.postId}</div>
         </div>
     )
 }
+
+export default page
+*/
+/*
+import react from "react"
+const page = async (props: any) => {
+    console.log(props)
+    return(
+        <div>
+            <div>ola</div>
+        </div>
+    )
+}
+export default page
+*/
+// mais informações https://www.youtube.com/watch?v=Sbl04kOL1dM aos 30:00 minutos
+//Posso transformar isso em um componente de servidor:
+const page = async (props:any) =>{
+    return (
+        <div>
+            <div>Ola</div>
+        </div>
+    )
+}
+/*
+// Isto abaixo é um exemplo que não existe mais
+export async function getServerSideProps() {
+    return(
+        props:{}
+    )
+}
+*/
+export default page
+//Para otimizar e deixar a página mais rápida ver o link https://www.youtube.com/watch?v=Sbl04kOL1dM no minuto 43:00
