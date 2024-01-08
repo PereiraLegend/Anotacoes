d34e545214f9864a20b41f541e9b014d524a01dd
Lucas <pereirapro123@gmail.com>
2024-01-08 18:29:20 -0300

    Commit

---
+++ APis Teste/api-aprendizado/src/database/conexao.js
@@ -0,0 +1,12 @@
+// Aqui possuirá todos os códigos necessários para se conectar a uma base de dados sql
+const mysql = require('mysql')
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'bdcopa'
+})
+
+conexao.connect()
+
+export default conexao //eu exporto a conexão pois irei utiliza-lo em outros lugares
