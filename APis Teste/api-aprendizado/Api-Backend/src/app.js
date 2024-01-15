f031866a8c5223bc136f8e1c343e976e48b81ef0
Lucas <pereirapro123@gmail.com>
2024-01-15 18:35:14 -0300

    Commit atualizações

--- APis Teste/api-aprendizado/Api-Backend/src/app.js
+++ APis Teste/api-aprendizado/Api-Backend/src/app.js
@@ -7,3 +7,14 @@ const app = express()
 app.use(cors())
 app.use(router)
 module.exports = app
+
+// Configurando o core para que a aplicação apenas seja acessada apenas pelo endereço do site em especifico
+/*
+const corsOptions = {
+    origin: ['http://seu-site-1.com', 'http://seu-site-2.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+*/
