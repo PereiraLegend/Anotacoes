d14ae3a68fc7a1c3588c9d8a2534ee68dbde36f2
Lucas <pereirapro123@gmail.com>
2024-04-18 18:32:51 -0300

    Commit

---
+++ Java Web - SpringBoot/todosimple/src/main/java/com/testelucas/todosimple/TodosimpleApplication.java
@@ -0,0 +1,13 @@
+package com.testelucas.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosimpleApplication.class, args);
+	}
+
+}
---
+++ Java Web - SpringBoot/todosimple/src/test/java/com/testelucas/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testelucas.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
